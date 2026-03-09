0cb5292aec079cf7c5821e3ceeeea82faaf44229
Vanishree <155431345+vani-6301@users.noreply.github.com>
2026-03-09 23:44:43 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // scan all React files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
